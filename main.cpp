d310a2be60bfb46902c3ee6633daa879c7851e5f
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-16 13:30:45 +0000

    notes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 //a variable is a location in which u store info
-// a pointer is a variable that holds memory
+// a pointer is a variable that holds memory that points to location
+// int holds 4 bytes
+//bool 1 byte
+//
 
 int main() {
 
