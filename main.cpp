828371a2692c8fb5846f97eedd5f2b8505149138
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-16 13:45:07 +0000

    notes - ternary and binary

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//a variable is a location in which u store info
-// a pointer is a variable that holds memory that points to location
-// int holds 4 bytes
-//bool 1 byte
-//
 
 int main() {
 
   
-  std::cout << "Hello World!\n";
+
 
 
 
