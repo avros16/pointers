41ff1ba48f3cc8216a5bee951a4d14b629d735ff
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-16 13:51:47 +0000

    ex 1

--- .ccls-cache/@home@runner@pointers/main.cpp
+++ .ccls-cache/@home@runner@pointers/main.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main() {
 
+  unsigned short shortVar{5};
+  unsigned long longVar{65535};
   
+ cout << "shortVar:\t" << shortVar;
+  cout << "\tAddress of shortVar:\t" << &longVar <<endl;
 
+
+  cout << "longVar:\t" << longVar;
+ cout << "\tAddress of longVar:\t" << longVar;
+  
 
 
 
