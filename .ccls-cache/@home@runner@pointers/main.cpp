f3a4797f017f7d57a3a8568ddbdb674e76731a6e
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-16 13:16:44 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@pointers/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//a variable is a location in which u store info
+// a pointer is a variable that holds memory
+
+int main() {
+
+  
+  std::cout << "Hello World!\n";
+
+
+
+  
+}
